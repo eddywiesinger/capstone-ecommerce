407dd661cc90d520f473af01312b8f73e24d333c
Eddy Wi <wiesinger.edwin@gmail.com>
2023-05-22 15:53:52 +0200

    add checkout route, article maintenance for admins

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import os
 from datetime import datetime
 
-import cloudinary
 from flask import Flask, render_template, flash, redirect, url_for, request, jsonify
 from flask_bootstrap import Bootstrap4
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from config import BaseConfig
 from decorators import admin_required
-
-from cloudinary.uploader import upload
-from cloudinary.utils import cloudinary_url
 
 import stripe
 
@@ -36,14 +34,6 @@ login_manager.login_view = "login"
 # DB
 db = SQLAlchemy(application)
 
-# CLOUDINARY
-cloudinary.config(
-    cloud_name=os.environ['CLOUDINARY_CLOUD_NAME'],
-    api_key=os.environ['CLOUDINARY_API_KEY'],
-    api_secret=os.environ['CLOUDINARY_API_SECRET'],
-    secure=True
-)
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -52,7 +42,7 @@ def load_user(user_id):
 
 ##CONFIGURE TABLES
 class User(db.Model, UserMixin):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
@@ -62,20 +52,20 @@ class User(db.Model, UserMixin):
     registered_on = db.Column(db.DateTime, nullable=False)
     mfa_secret = db.Column(db.String(250))
     first_login = db.Column(db.Boolean, nullable=False)
+    products = relationship('Product', back_populates='buyer')
     confirmed = db.Column(db.Boolean, nullable=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
 
 
-# class Article(db.Model):
-#     __tablename__ = "articles"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(250), unique=True, nullable=False)
-#     price = db.Column(db.Float, nullable=False)
-#     cloudinary_public_id = db.Column(db.String(500))
-#     cloudinary_secure_url = db.Column(db.String(500))
-#     cloudinary_transformed_url = db.Column(db.String(500))
-#     description = db.Column(db.Text)
-#     created_on = db.Column(db.DateTime, nullable=False)
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    buyer = relationship('User', back_populates='products')
+    buyer_id = db.Column(db.Integer, ForeignKey('users.id'))
+    stripe_product_id = db.Column(db.String(250), nullable=False)
+    stripe_price_id = db.Column(db.String(250), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    added_on = db.Column(db.DateTime)
 
 
 with application.app_context():
@@ -84,18 +74,13 @@ with application.app_context():
 
 @application.route('/')
 def home():
-    all_products = stripe.Product.list()
-    for product in all_products:
-        if product.default_price:
-            price = stripe.Price.retrieve(product.default_price)
-            product.price_amount = price.unit_amount_decimal
-    return render_template('home.html', articles=all_products)  # Article.query.all()
+    return render_template('home.html', articles=get_active_products())
 
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('home'))
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -183,14 +168,17 @@ def new_article():
             file_obj = stripe.File.create(
                 file=image_file,
                 purpose='dispute_evidence',
+                file_link_data={
+                    'create': True
+                }
             )
-            url = file_obj.url
+            url = file_obj.links.data[0].url
         stripe.Product.create(
             name=name,
             description=description,
             default_price_data={
                 'currency': 'EUR',
-                'unit_amount_decimal': price
+                'unit_amount': int(float(price) * 100)
             },
             images=[url]
         )
@@ -204,12 +192,7 @@ def new_article():
 @application.route('/articles')
 @admin_required
 def articles():
-    all_products = stripe.Product.list()
-    for product in all_products:
-        if product.default_price:
-            price = stripe.Price.retrieve(product.default_price)
-            product.price_amount = price.unit_amount_decimal
-    return render_template('admin/articles.html', articles=all_products)  # Article.query.all()
+    return render_template('admin/articles.html', articles=get_active_products())  # Article.query.all()
 
 
 @application.route('/article/<article_id>', methods=['GET', 'POST'])
@@ -217,7 +200,10 @@ def articles():
 def edit_article(article_id):
     article_to_edit = stripe.Product.retrieve(article_id)  # Article.query.get(int(article_id))
     current_price = stripe.Price.retrieve(article_to_edit.default_price)
-    article_to_edit.price_amount = current_price.unit_amount_decimal
+    try:
+        article_to_edit.price_amount = current_price.unit_amount / 100
+    except:
+        article_to_edit.price_amount = None
     if request.method == 'POST':
         name = request.form.get('name')
         description = request.form.get('description')
@@ -240,7 +226,7 @@ def edit_article(article_id):
             created_price = stripe.Price.create(
                 product=article_id,
                 currency='EUR',
-                unit_amount_decimal=price
+                unit_amount=int(float(price) * 100)
             )
             stripe.Product.modify(
                 article_id,
@@ -252,20 +238,18 @@ def edit_article(article_id):
                     active=False
                 )
         if image_file:
-            url = ''
-            if image_file:
-                file_obj = stripe.File.create(
-                    file=image_file,
-                    purpose='dispute_evidence',
-                    file_link_data={
-                        'create': True
-                    }
-                )
-                url = file_obj.links.data[0].url
-                stripe.Product.modify(
-                    article_id,
-                    images=[url]
-                )
+            file_obj = stripe.File.create(
+                file=image_file,
+                purpose='dispute_evidence',
+                file_link_data={
+                    'create': True
+                }
+            )
+            url = file_obj.links.data[0].url
+            stripe.Product.modify(
+                article_id,
+                images=[url]
+            )
 
         flash('Article was updated successfully!', 'success')
 
@@ -276,7 +260,14 @@ def edit_article(article_id):
 @application.route('/article/delete/<article_id>')
 @admin_required
 def delete_article(article_id):
-    stripe.Product.delete(article_id)
+    product_to_delete = stripe.Product.retrieve(article_id)
+    if product_to_delete.default_price:
+        stripe.Product.modify(
+            article_id,
+            active=False
+        )
+    else:
+        stripe.Product.delete(article_id)
 
     flash('Article was deleted successfully!', 'success')
     return redirect(url_for('home'))
@@ -294,27 +285,102 @@ def users():
     return render_template('admin/users.html', users=User.query.all())
 
 
+@application.route('/add-to-chart/<article_id>')
+@login_required
+def add_to_chart(article_id):
+    product_to_add = stripe.Product.retrieve(article_id)
+    new_product = Product(
+        buyer=current_user,
+        buyer_id=current_user.id,
+        stripe_product_id=article_id,
+        stripe_price_id=product_to_add.default_price,
+        quantity=1,
+        added_on=datetime.now()
+    )
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    flash('Successfully added product to chart.', 'success')
+    return redirect(url_for('home'))
+
+
+@application.route('/remove-from-chart/<article_id>')
+@login_required
+def remove_from_chart(article_id):
+    delete_q = Product.__table__.delete()\
+        .where(Product.stripe_product_id == article_id)\
+        .where(Product.buyer_id == current_user.id)
+    db.session.execute(delete_q)
+    db.session.commit()
+    flash('Successfully deleted product from chart.', 'success')
+    return redirect(url_for('checkout'))
+
+
 @application.route('/checkout', methods=['GET', 'POST'])
+@login_required
 def checkout():
+    checkout_products = Product.query.filter_by(buyer_id=current_user.id)
+    articles = []
+    for product in checkout_products:
+        stripe_product = stripe.Product.retrieve(product.stripe_product_id)
+        price = stripe.Price.retrieve(stripe_product.default_price).unit_amount
+        try:
+            stripe_product.price_amount = price / 100
+            stripe_product.total_price = price / 100 * int(product.quantity)
+        except:
+            stripe_product.price_amount = None
+            stripe_product.total_price = None
+        stripe_product.quantity = product.quantity
+        articles.append(stripe_product)
     if request.method == 'POST':
+        buy_items = []
+        for product in checkout_products:
+            stripe_product = stripe.Product.retrieve(product.stripe_product_id)
+            buy_items.append({
+                'price': stripe_product.default_price,
+                'quantity': product.quantity
+            })
         try:
             checkout_session = stripe.checkout.Session.create(
-                line_items=[
-                    {
-                        # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                        'price': 'price_1N9oW6BXXPZuK6COQbRpcUei',
-                        'quantity': 1,
-                    },
-                ],
+                line_items=buy_items,
                 mode='payment',
-                success_url=YOUR_DOMAIN + '/success.html',  # url_for('home'),
-                cancel_url=YOUR_DOMAIN + '/cancel.html',  # url_for('checkout'),
+                success_url=YOUR_DOMAIN + url_for('checkout_success'),
+                cancel_url=YOUR_DOMAIN + url_for('checkout_cancel'),
             )
         except Exception as e:
             return str(e)
 
         return redirect(checkout_session.url, code=303)
-    return render_template('checkout.html')
+    return render_template('checkout.html', products=articles)
+
+
+@application.route('/checkout/cancel')
+def checkout_cancel():
+    flash('Checkout was cancelled', 'warning')
+    return redirect(url_for('checkout'))
+
+
+@application.route('/checkout/success')
+def checkout_success():
+    flash('Checkout successful. Thank you for your purchase!', 'success')
+    delete_q = Product.__table__.delete().where(Product.buyer_id == current_user.id)
+    db.session.execute(delete_q)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+# HELPER
+def get_active_products():
+    all_products = stripe.Product.list(active=True)
+    for product in all_products:
+        if product.default_price:
+            price = stripe.Price.retrieve(product.default_price)
+            try:
+                product.price_amount = price.unit_amount / 100
+            except:
+                product.price_amount = None
+    return all_products
 
 
 if __name__ == '__main__':
